705d564b414d64bd8edb2ce2e490acf779a4f6bc
Devansh Sharma <devanshneeraj123@gmail.com>
2023-01-08 17:29:07 +0530

    adding new changes

--- scripts/run.js
+++ scripts/run.js
@@ -1,21 +1,26 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
+  //const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-  const domainContract = await domainContractFactory.deploy();
+  //We're passing "shards" to the constructor when deploying 
+  const domainContract = await domainContractFactory.deploy("shards");
   await domainContract.deployed();
+
   console.log("Contract deployed to:", domainContract.address);
-  console.log("Contract deployed by:", owner.address);
-  
-  const txn = await domainContract.register("Devansh");
+  //console.log("Contract deployed by:", owner.address);
+
+  //We're passing in a 2nd variable - value. This is the moneyyyyyyyy  
+  let txn = await domainContract.register("Devansh", {value: hre.ethers.utils.parseEther('0.1')});
   await txn.wait();
 
-  const domainAddress = await domainContract.getAddress("Devansh");
-  console.log("Owner of domain:", domainAddress);
+  const address = await domainContract.getAddress("Devansh");
+  console.log("Owner of domain:", address);
 
   // Trying to set a record that doesn't belong to me !
-  const txn1 = await domainContract.connect(randomPerson).setRecord("Devansh","Haha my domain now!");
-  await txn1.wait();
+  /*const txn1 = await domainContract.connect(randomPerson).setRecord("Devansh","Haha my domain now!");
+  await txn1.wait();*/
 
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 
 };
 
